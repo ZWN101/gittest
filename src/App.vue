1c9a961dc5b8805fa2826b10f0bc9ff4aca23460
ZWN101 <zwn5350@163.com>
2021-01-16 16:59:17 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
@@ -17,12 +14,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
